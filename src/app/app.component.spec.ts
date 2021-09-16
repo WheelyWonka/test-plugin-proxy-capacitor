f6f0aedac3cf8dfeb6a56ba3c7294fb802c77858
William Thibault <william.thibault@hiloenergie.com>
2021-09-16 10:03:15 -0400

    add test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
+import { App, AppInfo } from '@capacitor/app';
+import { Capacitor } from '@capacitor/core';
 
 import { AppComponent } from './app.component';
 
@@ -13,11 +15,24 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should get app info version', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.debugElement.componentInstance;
+
+    spyOn(Capacitor, 'isNativePlatform').and.returnValue(true);
+    spyOn(App, 'getInfo').and.resolveTo({version: '9.9.9'} as AppInfo);
+
+    component.ngOnInit();
+
+    // We can see here that App is a Proxy.
+    console.log(App);
+
+    // Capacitor isNativePlatform is correclty spyied on, because not a Proxy ?
+    expect(Capacitor.isNativePlatform()).toBeTrue();
+
+    // Here, App getInfo is not spied on, because is a Proxy ?
+    // Error: <toHaveBeenCalled> : Expected a spy, but got Function.
+    expect(App.getInfo).toHaveBeenCalled();
   });
-  // TODO: add more tests!
 
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { App } from '@capacitor/app';
+import { Capacitor } from '@capacitor/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  appInfoVersion = 'Unavailable';
+
   constructor() {}
+
+  async ngOnInit() {
+    if (Capacitor.isNativePlatform()) {
+      this.appInfoVersion = (await App.getInfo()).version;
+    }
+  }
 }
